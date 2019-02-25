76924f3d0913b3c3e6391f82055d11c565c42ae5
eddie barasa <eddiebarasa.20@gmail.com>
2019-02-25 19:07:00 +0300

    all changes

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from users import views as user_views
 
 urlpatterns = [
+	path('register/', user_views.register, name='register'),
     path('', views.home, name='blog-home'),
     path('about/', views.about, name='blog-about'),
     path('services/', views.services, name='blog-services'),
