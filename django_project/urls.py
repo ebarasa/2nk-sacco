2e50357bed5e69e09b522d8d06b31002c6897a09
eddie barasa <eddiebarasa.20@gmail.com>
2019-02-28 20:02:13 +0300

    all changes

--- django_project/urls.py
+++ django_project/urls.py
@@ -21,8 +21,12 @@ from users import views as user_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('register', user_views.register, name='register'),
-    path('login', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', user_views.register, name='register'),
+    path('report/', user_views.report, name='report'),
+    path('profile/', user_views.profile, name='profile'),
+    path('activities/', user_views.activities, name='activities'),
+    path('details/', user_views.details, name='details'),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages 
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 def register(request):
@@ -13,4 +14,17 @@ def register(request):
 	else:
 		form = UserRegisterForm()
 	return render(request, 'users/register.html', {'form':form})
- 
+
+
+@login_required
+def profile(request):
+	return render(request, 'users/profile.html')
+
+def activities(request):
+	return render(request, 'users/activities.html')
+
+def details(request):
+	return render(request, 'users/details.html')
+
+def report(request):
+	return render(request, 'users/report.html')
