68082e25a57ddfe3a50e5a66b54ee595282a1921
eddie barasa <eddiebarasa.20@gmail.com>
2019-03-02 16:54:23 +0300

    all changes

--- users/forms.py
+++ users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -14,7 +15,34 @@ class UserRegisterForm(UserCreationForm):
 	matatu_registration_number = forms.CharField(required=True)
 	location = forms.CharField(required=True)
 
-
 	class Meta:
 		model = User
 		fields = ['username', 'first_name', 'middle_name',  'last_name', 'national_Id',  'email', 'mobile_number', 'matatu_name', 'matatu_registration_number', 'location', 'password1', 'password2']
+
+
+
+class UserUpdateForm(forms.ModelForm):
+	first_name = forms.CharField(required=True)
+	middle_name = forms.CharField(required=True)
+	last_name = forms.CharField(required=True)
+	national_Id = forms.CharField(required=True)
+	email = forms.EmailField(required=True)
+	mobile_number = forms.CharField(required=True)
+	matatu_name = forms.CharField(required=True) 
+	matatu_registration_number = forms.CharField(required=True)
+	location = forms.CharField(required=True)
+
+
+
+	class Meta:
+		model = User
+		fields = ['username', 'first_name', 'middle_name',  'last_name', 'national_Id',  'email', 'mobile_number', 'matatu_name', 'matatu_registration_number', 'location']
+
+
+class ProfileUpdateForm(forms.ModelForm):
+	class Meta:
+		model = Profile
+		fields = ['image']
+
+	
+
