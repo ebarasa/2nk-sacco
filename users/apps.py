2174372305c60a1ea794356ee4cf7b6905a11191
eddie barasa <eddiebarasa.20@gmail.com>
2019-03-02 13:34:34 +0300

    all changes

--- users/apps.py
+++ users/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class UsersConfig(AppConfig):
     name = 'users'
+
+
+    def ready(self):
+    	# import pdb; pdb.set_trace()
+    	import users.signals 
+
+
+
+  
